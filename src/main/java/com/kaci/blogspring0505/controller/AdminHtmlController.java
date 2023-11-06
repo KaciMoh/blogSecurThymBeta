61235fd16bf5580943d7f53212252b09045ec5d4
Muhend <hamohand@gmail.com>
2023-11-06 11:57:32 +0100

    Validations des comptes

--- src/main/java/com/kaci/blogspring0505/controller/AdminHtmlController.java
+++ src/main/java/com/kaci/blogspring0505/controller/AdminHtmlController.java
@@ -22,10 +22,10 @@ public class AdminHtmlController {
     private IAdminService iAdminService;
 
     /*** Compte ****************/
-    //READ
-    //Accueil modérateur
+    // READ
+    // Accueil modérateur
     @GetMapping("/admin/index")
-    public String indexAdmin(){
+    public String indexAdmin() {
         return "indexAdmin";
     }
 
@@ -38,39 +38,20 @@ public class AdminHtmlController {
     }
 
     // UPDATE
-    // Valider un compte ************************************************
-    @PutMapping("/admin/validecompte")
-    public String valideCompte(@RequestParam("idCompte") Long idCompte){
+    // Valider un compte
+    @GetMapping("/admin/validecompte")
+    public String valideCompte(@RequestParam("idCompte") Long idCompte) {
         iAdminService.validerCompte(idCompte);
-        //Compte compte = iAdminService.chercheCompte(idCompte);
-        //compte.setValide(true);
-        return "redirect:/redact/index";
-    }
-
-    // 1. modifier un compte
-    @GetMapping("/admin/editCompte") // édite le nouveau contenu d'un compte
-    public String editCompte(Model model, @RequestParam("idCompte") Long idCompte) {
-        try {
-            Compte compte = iAdminService.chercheCompte(idCompte);
-            model.addAttribute("compte", compte);
-            return "comptes/editCompte"; // formulaire de modification du compte
-            //System.out.println("OK_OK");
-            //return "OK_OK"; // formulaire de modification du compte
-        } catch (RuntimeException e) { // cas erreur
-           return "redirect:/admin/listecomptesnonvalide"; // retour à l'accueil
-            //System.out.println("ERREUR");
-
-        }
-    }
-    // 2. Enregistrer les modifications
-    @PostMapping("/admin/saveModifCompte") // sauvegarde le compte avec les modifs
-    public String saveModifCompte(Compte compte, Long id) { //
-        //iRedacteurService.creeArticle(article);
-        compte.setValide(true); //compte validé
-        iAdminService.modifieCompte(compte,id);
         return "redirect:/admin/listecomptesnonvalide";
     }
 
-
+    // 2. Enregistrer les modifications
+    @PostMapping("/admin/saveModifCompte") // sauvegarde le compte avec les modifs
+    public String saveModifCompte(Compte compte, Long id) { //
+        // iRedacteurService.creeArticle(article);
+        compte.setValide(true); // compte validé
+        iAdminService.modifieCompte(compte, id);
+        return "redirect:/admin/listecomptesnonvalide";
+    }
 
 }

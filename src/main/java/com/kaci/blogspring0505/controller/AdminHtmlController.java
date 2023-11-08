2fce81121ae43cc985bd2b2e98d81bb5b54a648e
Muhend <hamohand@gmail.com>
2023-11-08 20:48:28 +0100

    validation comptes

--- src/main/java/com/kaci/blogspring0505/controller/AdminHtmlController.java
+++ src/main/java/com/kaci/blogspring0505/controller/AdminHtmlController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Date;
 import java.util.List;
@@ -35,21 +36,19 @@ public class AdminHtmlController {
         return "comptes/listeComptes";
     }
 
+    // Liste des comptes non validés
+    @GetMapping("/admin/listecomptesnonvalide") //
+    public String listeCompteNonValide(Model model) {
+        List<Compte> comptes = iAdminService.listeComptesNonValides();
+        model.addAttribute("liste", comptes);
+        return "comptes/listeComptesNonValide";
+    }
+
     //
     // Valider un compte
     @GetMapping("/admin/validecompte")
     public String valideCompte(@RequestParam("idCompte") Long idCompte) {
         iAdminService.validerCompte(idCompte);
-        return "redirect:/admin/listecomptesnonvalide";
+        return "comptes/messages/compteValide";
     }
-
-    // 2. Enregistrer les modifications
-    @PostMapping("/admin/saveModifCompte") // sauvegarde le compte avec les modifs
-    public String saveModifCompte(Compte compte, Long id) { //
-        // iRedacteurService.creeArticle(article);
-        compte.setValide(true); // compte validé
-        iAdminService.modifieCompte(compte, id);
-        return "redirect:/admin/listecomptesnonvalide";
-    }
-
 }
